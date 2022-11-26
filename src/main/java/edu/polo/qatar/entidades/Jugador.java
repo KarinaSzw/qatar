abbe0b37dda613f5d511b33b1d7e1411aab30575
IvoMart <ivo_ivancho@hotmail.com>
2022-11-26 08:41:25 -0300

    Actualizar Clase Jugador con la extensión de la clase Persona

--- src/main/java/edu/polo/qatar/entidades/Jugador.java
+++ src/main/java/edu/polo/qatar/entidades/Jugador.java
@@ -1,33 +1,23 @@
 package edu.polo.qatar.entidades;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import java.util.*;
+import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
-
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
+@EqualsAndHashCode(callSuper=true)
 @Table(name="jugadores")
-public class Jugador {
+public class Jugador extends Persona implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank(message = "Campo obligatorio")
-    @Size(max = 250, message= "Nombre demasiado largo")
-    private String nombre;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @Temporal(TemporalType.DATE)
-    @NotNull(message = "Campo obligatorio")
-    @Past(message = "La fecha de nacimiento no puede ser futura")
-    private Date fechaNacimiento;
 
     @ManyToOne
     @JsonBackReference
@@ -36,4 +26,38 @@ public class Jugador {
 
     private String foto;
 
+    public Jugador(String nombre, Date fechaNacimiento, Pais lugarDeNacimiento) {
+        super(nombre, fechaNacimiento, lugarDeNacimiento);
+    }
+
+    public Jugador(Long id, Seleccion seleccion, String foto, String nombre, Date fechaNacimiento, Pais lugarDeNacimiento) {
+        super(nombre, fechaNacimiento, lugarDeNacimiento);
+        this.id = id;
+        this.seleccion = seleccion;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Pais getLugarDeNacimiento() {
+        return lugarDeNacimiento;
+    }
+
+    public void setLugarDeNacimiento(Pais lugarDeNacimiento) {
+        this.lugarDeNacimiento = lugarDeNacimiento;
+    }
 }
